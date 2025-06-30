f28eb893da019721d308a6962869b2d49e76c4fe
Astrof123 <valeev_101@bk.ru>
2025-06-30 22:44:44 +0800

    dwedwe

--- infa.ts
+++ infa.ts
@@ -1,9 +1,9 @@
-function numbersToDaysOfWeek(numbersString) {
-  // 1. Разбиваем строку на массив чисел.
-  const numbersArray = numbersString.split(',').map(Number).map(num => num.toString().trim());
+function numbersToDaysOfWeek(numbersString: string): string {
+  // 1. Разбиваем строку на массив чисел и удаляем пробелы.
+  const numbersArray: string[] = numbersString.split(',').map(num => num.trim());
 
   // 2. Определяем соответствие между числами и днями недели.
-  const dayOfWeekMap = {
+  const dayOfWeekMap: { [key: string]: string } = {
     '0': 'Понедельник',
     '1': 'Вторник',
     '2': 'Среда',
@@ -14,7 +14,7 @@ function numbersToDaysOfWeek(numbersString) {
   };
 
   // 3. Преобразуем числа в дни недели и собираем их в новый массив.
-  const daysOfWeekArray = numbersArray.map(number => {
+  const daysOfWeekArray: string[] = numbersArray.map(number => {
     if (dayOfWeekMap.hasOwnProperty(number)) {
       return dayOfWeekMap[number];
     } else {
@@ -23,16 +23,16 @@ function numbersToDaysOfWeek(numbersString) {
   });
 
   // 4. Соединяем дни недели в строку, разделенную запятыми.
-  const daysOfWeekString = daysOfWeekArray.join(', ');
+  const daysOfWeekString: string = daysOfWeekArray.join(', ');
 
   return daysOfWeekString;
 }
 
 // Пример использования:
-const inputString = "0, 2, 4, 6";
-const outputString = numbersToDaysOfWeek(inputString);
+const inputString: string = "0, 2, 4, 6";
+const outputString: string = numbersToDaysOfWeek(inputString);
 console.log(outputString); // Вывод: Понедельник, Среда, Пятница, Воскресенье
 
-const invalidInput = "0, 2, 7, 4";
-const invalidOutput = numbersToDaysOfWeek(invalidInput);
+const invalidInput: string = "0, 2, 7, 4";
+const invalidOutput: string = numbersToDaysOfWeek(invalidInput);
 console.log(invalidOutput); // Вывод: Понедельник, Среда, Некорректное число (7), Пятница
